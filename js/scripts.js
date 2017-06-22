337a11585e3f4ae445d26e1971ed95e263779e5a
tomasz <tomasztargo@gmail.com>
2017-06-22 08:43:43 +0200

    poprawione warunki

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
 // scripts.js
 //zmienne
-var a = -3,
-	b = 5,
+var a = 1,
+	b = 2,
 	value = (a * a) + (2 * a * b) - (b * b);
 
 // informacja w konsoli
 console.log('Result= ' + value);
 
-// sprawdzanie czy dodatni, czy ujemny
-var plusOrMinus = value > 0 ? 'wynik jest dodatni' : 'Wynik jest ujemny';
-console.log(plusOrMinus);
-
-//sprawdzanie czy równy 0
-if ( value == 0 ) {
-    console.log('wynik jest równy: \"0\"')
+//sprawdzanie warunków
+if ( value > 0 ) {
+    console.log('wynik jest dodatni')
+} else if ( value < 0 ){
+	console.log('Wynik jest ujemny')
 } else {
-	console.log('wynik jest różny od: \"0\"')
+	console.log('wynik jest równy: \"0\"')
 }
